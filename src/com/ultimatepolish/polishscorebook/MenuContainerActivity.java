ed0cda01620e2d1c06e82e2f30abc278e062d158
Phillip Anderson <anderson.pa@gmail.com>
2013-11-13 17:54:49 +0000

    some code cleanup. moved log methods to MenuContainerActivity since most classes extend that anyway.

--- src/com/ultimatepolish/polishscorebook/MenuContainerActivity.java
+++ src/com/ultimatepolish/polishscorebook/MenuContainerActivity.java
@@ -2,6 +2,7 @@ package com.ultimatepolish.polishscorebook;
 
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,7 +10,8 @@ import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 import com.ultimatepolish.scorebookdb.DatabaseHelper;
 
 public class MenuContainerActivity extends OrmLiteBaseActivity<DatabaseHelper> {
-	
+	public static String LOGTAG = "MenuContainer";
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -19,74 +21,98 @@ public class MenuContainerActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-		    case android.R.id.home:
-	            NavUtils.navigateUpFromSameTask(this);
-	            return true;
-		    case R.id.addButton:
-	            openAddActivity();
-	            return true;
-	        case R.id.games:
-	            openGamesActivity();
-	            return true;
-	        case R.id.players:
-	            openPlayersActivity();
-	            return true;
-	        case R.id.teams:
-	            openTeamsActivity();
-	            return true;
-	        case R.id.venues:
-	            openVenuesActivity();
-	            return true;
-	        case R.id.sessions:
-	            openSessionsActivity();
-	            return true;
-	        case R.id.modifyButton:
-	            openModifyActivity();
-	            return true;
-	        case R.id.settings:
-	            openSettingsActivity();
-	            return true;
-	        case R.id.about:
-	            openAboutActivity();
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		case R.id.addButton:
+			openAddActivity();
+			return true;
+		case R.id.games:
+			openGamesActivity();
+			return true;
+		case R.id.players:
+			openPlayersActivity();
+			return true;
+		case R.id.teams:
+			openTeamsActivity();
+			return true;
+		case R.id.venues:
+			openVenuesActivity();
+			return true;
+		case R.id.sessions:
+			openSessionsActivity();
+			return true;
+		case R.id.modifyButton:
+			openModifyActivity();
+			return true;
+		case R.id.settings:
+			openSettingsActivity();
+			return true;
+		case R.id.about:
+			openAboutActivity();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
+
 	public void openAddActivity() {
-    	// this wont do anything unless overridden in the file that extends this class
-    }
+		// this wont do anything unless overridden in the file that extends this
+		// class
+	}
+
 	public void openModifyActivity() {
-    	// this wont do anything unless overridden in the file that extends this class
-    }
-    public void openGamesActivity() {
-    	Intent intent = new Intent(this, View_Games.class);
-    	startActivity(intent);
-    }
-    public void openPlayersActivity() {
-    	Intent intent = new Intent(this, View_Players.class);
-    	startActivity(intent);
-    }
-    public void openTeamsActivity() {
-    	Intent intent = new Intent(this, View_Teams.class);
-    	startActivity(intent);
-    }
-    public void openVenuesActivity(){
-    	Intent intent = new Intent(this, View_Venues.class);
-    	startActivity(intent);
-    }
-    public void openSessionsActivity(){
-    	Intent intent = new Intent(this, View_Sessions.class);
-    	startActivity(intent);
-    }
-    public void openSettingsActivity(){
-    	Intent intent = new Intent(this, SimpleSettings.class);
-    	startActivity(intent);
-    }
-    public void openAboutActivity() {
-    	Intent intent = new Intent(this, AboutPage.class);
-    	startActivity(intent);
-    }
+		// this wont do anything unless overridden in the file that extends this
+		// class
+	}
+
+	public void openGamesActivity() {
+		Intent intent = new Intent(this, View_Games.class);
+		startActivity(intent);
+	}
+
+	public void openPlayersActivity() {
+		Intent intent = new Intent(this, View_Players.class);
+		startActivity(intent);
+	}
+
+	public void openTeamsActivity() {
+		Intent intent = new Intent(this, View_Teams.class);
+		startActivity(intent);
+	}
+
+	public void openVenuesActivity() {
+		Intent intent = new Intent(this, View_Venues.class);
+		startActivity(intent);
+	}
+
+	public void openSessionsActivity() {
+		Intent intent = new Intent(this, View_Sessions.class);
+		startActivity(intent);
+	}
+
+	public void openSettingsActivity() {
+		Intent intent = new Intent(this, SimpleSettings.class);
+		startActivity(intent);
+	}
+
+	public void openAboutActivity() {
+		Intent intent = new Intent(this, AboutPage.class);
+		startActivity(intent);
+	}
+
+	public void log(String msg) {
+		Log.i(LOGTAG, msg);
+	}
+
+	public void logd(String msg) {
+		Log.d(LOGTAG, msg);
+	}
+
+	public void loge(String msg, Exception e) {
+		Log.e(LOGTAG, msg + ": " + e.getMessage());
+	}
+
 }
