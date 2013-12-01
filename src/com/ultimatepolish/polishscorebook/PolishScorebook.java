c88c1e578c14f7a3424a1d0f8b7c244b9ba08998
Phillip Anderson <anderson.pa@gmail.com>
2013-12-01 12:35:47 +0000

    added a navigation drawer. not functional yet, so actionbar items are retained for now

--- src/com/ultimatepolish/polishscorebook/PolishScorebook.java
+++ src/com/ultimatepolish/polishscorebook/PolishScorebook.java
@@ -1,28 +1,202 @@
 package com.ultimatepolish.polishscorebook;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class PolishScorebook extends  MenuContainerActivity {
+public class PolishScorebook extends MenuContainerActivity {
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_polish_scorebook);
-    }
-    
-    @Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+	private String[] mItemTitles;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_polish_scorebook);
+
+		mTitle = mDrawerTitle = getTitle();
+		mItemTitles = getResources().getStringArray(R.array.menuItems);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// custom shadow that overlays the main content when the drawer opens
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+				GravityCompat.START);
+		// set up the drawer's list view with items and click listener
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, mItemTitles));
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the sliding drawer and the action bar app icon
+		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+		mDrawerLayout, /* DrawerLayout object */
+		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+		R.string.drawer_open, /* "open drawer" description for accessibility */
+		R.string.drawer_close /* "close drawer" description for accessibility */
+		) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		if (savedInstanceState == null) {
+			selectItem(0);
+		}
 	}
-    
-    public void openNewGameActivity(View view){
-    	Intent intent = new Intent(this, NewGame.class);
-    	startActivity(intent);
-    }
+
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// MenuInflater inflater = getMenuInflater();
+	// inflater.inflate(R.menu.main, menu);
+	// return super.onCreateOptionsMenu(menu);
+	// }
+
+	/* Called whenever we call invalidateOptionsMenu() */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// hide action items when drawer is open
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.games).setVisible(!drawerOpen);
+		menu.findItem(R.id.players).setVisible(!drawerOpen);
+		menu.findItem(R.id.teams).setVisible(!drawerOpen);
+		menu.findItem(R.id.sessions).setVisible(!drawerOpen);
+		menu.findItem(R.id.venues).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// The action bar home/up action should open or close the drawer.
+		// ActionBarDrawerToggle will take care of this.
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle action buttons
+		switch (item.getItemId()) {
+		// case R.id.action_websearch:
+		// // create intent to perform web search for this planet
+		// Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+		// intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
+		// // catch event that there's no activity to handle intent
+		// if (intent.resolveActivity(getPackageManager()) != null) {
+		// startActivity(intent);
+		// } else {
+		// Toast.makeText(this, R.string.app_not_available,
+		// Toast.LENGTH_LONG).show();
+		// }
+		// return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/* The click listener for ListView in the navigation drawer */
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			selectItem(position);
+		}
+	}
+
+	private void selectItem(int position) {
+		// update the main content by replacing fragments
+		Fragment fragment = new ContentFragment();
+		// Bundle args = new Bundle();
+		// args.putInt(ContentFragment.ARG_PLANET_NUMBER, position);
+		// fragment.setArguments(args);
+
+		FragmentManager fragmentManager = getFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment).commit();
+
+		// update selected item and title, then close the drawer
+		mDrawerList.setItemChecked(position, true);
+		setTitle(mItemTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
+
+	/**
+	 * When using the ActionBarDrawerToggle, you must call it during
+	 * onPostCreate() and onConfigurationChanged()...
+	 */
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggles
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	/**
+	 * Fragment that appears in the "content_frame", shows a planet
+	 */
+	public static class ContentFragment extends Fragment {
+		public static final String ARG_PLANET_NUMBER = "planet_number";
+
+		public ContentFragment() {
+			// Empty constructor required for fragment subclasses
+		}
+
+		// @Override
+		// public View onCreateView(LayoutInflater inflater, ViewGroup
+		// container,
+		// Bundle savedInstanceState) {
+		// View rootView = inflater.inflate(R.layout.fragment_planet,
+		// container, false);
+		// int i = getArguments().getInt(ARG_PLANET_NUMBER);
+		// String planet = getResources()
+		// .getStringArray(R.array.planets_array)[i];
+		//
+		// int imageId = getResources().getIdentifier(
+		// planet.toLowerCase(Locale.getDefault()), "drawable",
+		// getActivity().getPackageName());
+		// ((ImageView) rootView.findViewById(R.id.image))
+		// .setImageResource(imageId);
+		// getActivity().setTitle(planet);
+		// return rootView;
+		// }
+	}
 }
