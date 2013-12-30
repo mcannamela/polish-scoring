8d85b1ff536301d286a9257788e4d10d41dca456
Phillip Anderson <anderson.pa@gmail.com>
2013-12-30 04:34:19 +0000

    a lot of progress splitting up bracket stuff into logical parts.

--- src/com/ultimatepolish/enums/BrNodeType.java
+++ src/com/ultimatepolish/enums/BrNodeType.java
@@ -2,14 +2,15 @@ package com.ultimatepolish.enums;
 
 /** Enum for the nodes in a bracket */
 public final class BrNodeType {
-	// These are the different types
-	public static final int TIP = 0;
-	public static final int WIN = 1;
-	public static final int LOSS = 2;
-	public static final int BYE = 3;
-	public static final int UNSET = 4;
-	public static final int RESPAWN = 5;
-	public static final int NA = 7;
+	// These are the different types. Negative numbers used to avoid conflict
+	// with seed numbers.
+	public static final int TIP = -1;
+	public static final int WIN = -2;
+	public static final int LOSS = -3;
+	public static final int BYE = -4;
+	public static final int UNSET = -5;
+	public static final int RESPAWN = -6;
+	public static final int NA = -7;
 
 	// These are used to modify viewIds for easy classification
 	public static final int UPPER = 1000;
