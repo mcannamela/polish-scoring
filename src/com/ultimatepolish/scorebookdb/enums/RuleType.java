5e5c63f8f9b5fc6ce272e7bcbce7f13bcf27475e
mcannamela <m.cannamela@gmail.com>
2014-04-12 20:00:50 -0400

    add ruleset 002, where bottles are buffed and stalwarts are nerfed

--- src/com/ultimatepolish/scorebookdb/enums/RuleType.java
+++ src/com/ultimatepolish/scorebookdb/enums/RuleType.java
@@ -3,6 +3,7 @@ package com.ultimatepolish.scorebookdb.enums;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.ultimatepolish.scorebookdb.rulesets.RuleSet02;
 import com.ultimatepolish.scorebookdb.rulesets.RuleSet;
 import com.ultimatepolish.scorebookdb.rulesets.RuleSet00;
 import com.ultimatepolish.scorebookdb.rulesets.RuleSet01;
@@ -10,10 +11,12 @@ import com.ultimatepolish.scorebookdb.rulesets.RuleSet01;
 public final class RuleType {
 	public static final RuleSet RS00 = new RuleSet00();
 	public static final RuleSet RS01 = new RuleSet01();
+	public static final RuleSet RS02 = new RuleSet02();
 
 	public static final int rsNull = -1; // for sessions, no default rule set
 	public static final int rs00 = 0;
 	public static final int rs01 = 1;
+	public static final int rs02 = 2;
 
 	public static final Map<Integer, RuleSet> map = new HashMap<Integer, RuleSet>() {
 		/**
@@ -24,6 +27,7 @@ public final class RuleType {
 		{
 			put(0, new RuleSet00());
 			put(1, new RuleSet01());
+			put(2, new RuleSet02());
 		}
 	};
 }
